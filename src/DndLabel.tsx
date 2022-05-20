a6391212bd87ac9e214eef664a93914061c2b5dd
tkyk.niimura <tkyk.niimura@gmail.com>
2022-05-20 23:20:36 +0900

    fix

--- src/DndLabel.tsx
+++ src/DndLabel.tsx
@@ -1,7 +1,17 @@
 import { VFC } from 'react';
 
+const center = {
+  margin: 'auto',
+  top: 0,
+  left: 0,
+  right: 0,
+  bottom: 0,
+  position: 'absolute' as const,
+  height: '20px',
+};
+
 const DndTextArea: VFC<{ componentName: string }> = (props) => {
-  return <span style={{ verticalAlign: 'middle' }}>{props.componentName}</span>;
+  return <span style={center}>{props.componentName}</span>;
 };
 
 export default DndTextArea;
